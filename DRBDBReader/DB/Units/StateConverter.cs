051369cab0d62b7037d2070b2567050a5ef0e4b1
Kyle Repinski <repinski23@gmail.com>
2017-01-13 15:41:46 -0600

    Cleanup unused using statements

--- DRBDBReader/DB/Units/StateConverter.cs
+++ DRBDBReader/DB/Units/StateConverter.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * DRBDBReader
- * Copyright (C) 2016, Kyle Repinski
+ * Copyright (C) 2016-2017, Kyle Repinski
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-using System;
 using System.Collections.Generic;
 
 namespace DRBDBReader.DB.Units
--- DRBDBReader/frmMain.cs
+++ DRBDBReader/frmMain.cs
@@ -17,7 +17,6 @@
  */
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
 using System.Windows.Forms;
 using DRBDBReader.DB;
