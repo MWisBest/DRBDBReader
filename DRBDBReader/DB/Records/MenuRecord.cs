58bdc88b11ad023383cfd1ec138f43f75c4cb15f
Kyle Repinski <repinski23@gmail.com>
2017-01-19 10:17:33 -0600

    MenuRecord: Document additional fields

--- DRBDBReader/DB/Records/MenuRecord.cs
+++ DRBDBReader/DB/Records/MenuRecord.cs
@@ -20,17 +20,33 @@ namespace DRBDBReader.DB.Records
 	public class MenuRecord : Record
 	{
 		private const byte FIELD_ID = 0;
+		private const byte FIELD_PARENT_ID = 1;
 		private const byte FIELD_NAME_ID = 2;
+		private const byte FIELD_THREE = 3;
+		private const byte FIELD_SCREEN_POS = 4;
+		private const byte FIELD_FIVE = 5;
+		private const byte FIELD_EMPTY = 6;
 
 		public ushort id;
 
+		public ushort parentid;
+
 		public string name;
 		public ushort nameid;
+
+		public ushort fieldThree;
+
+		public byte screenpos;
+
+		public ushort fieldFive;
 
 		public MenuRecord( Table table, byte[] record ) : base( table, record )
 		{
 			// get id
 			this.id = (ushort)this.table.readField( this, FIELD_ID );
+
+			// get parentid
+			this.parentid = (ushort)this.table.readField( this, FIELD_PARENT_ID );
 
 			// get name
 			this.nameid = (ushort)this.table.readField( this, FIELD_NAME_ID );
@@ -40,6 +56,20 @@ namespace DRBDBReader.DB.Records
 				temp = "(null)";
 			}
 			this.name = temp;
+
+			// get fieldThree
+			// Seems important... likely points to an entry in a different table
+			this.fieldThree = (ushort)this.table.readField( this, FIELD_THREE );
+
+			// get screenpos
+			// This field is kind of strange.
+			// Row 0 will appear as two 00 bytes, Row 1 will appear as two 01 bytes, etc.
+			// They always match though, so we'll just ignore the extra byte.
+			this.screenpos = (byte)(this.table.readField( this, FIELD_SCREEN_POS ) & 0xFF);
+
+			// get fieldFive
+			// Unknown. Only values are 0 and 9.
+			this.fieldFive = (ushort)this.table.readField( this, FIELD_FIVE );
 		}
 	}
 }
